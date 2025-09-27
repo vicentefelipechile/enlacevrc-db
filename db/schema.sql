756320104b14561f5c9ae7aaa73e4d231b1c42be
Vicente <70909800+vicentefelipechile@users.noreply.github.com>
2025-09-27 00:37:32 -0300

    feat: added sql files

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS profiles;
+
+CREATE TABLE IF NOT EXISTS profiles (
+    vrchat_id       TEXT PRIMARY KEY,
+    discord_id      TEXT NOT NULL,
+    vrchat_name     TEXT NOT NULL,
+    added_at        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_banned       BOOLEAN NOT NULL DEFAULT FALSE,
+    banned_at       TIMESTAMP NULL,
+    banned_reason   TEXT NULL
+);
